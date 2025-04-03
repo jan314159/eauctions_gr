72a4c1eddccfab73b88aaa252f88ce6539519ca4
jan.pitonak <janpitonak@gmail.com>
2025-04-03 13:52:26 +0200

    added connection to DB for borrowers and mailing list
    updated requirements.txt

--- eauctions_scraper.py
+++ eauctions_scraper.py
@@ -50,6 +50,7 @@ class GrAuctionsScraper:
         options.add_argument("--headless")
         options.add_argument("--disable-blink-features=AutomationControlled")
         options.add_argument("--incognito")
+        options.add_argument("--no-sandbox")
         # options.add_argument('user-agent={userAgent}')
 
         custom_user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36"
