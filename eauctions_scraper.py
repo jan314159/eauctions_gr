62f6b13f22d63e20dd162bee166c8299b1be3f38
jan.pitonak <janpitonak@gmail.com>
2025-06-05 14:39:51 +0200

    added connection to DB for borrowers and mailing list
    updated requirements.txt

--- eauctions_scraper.py
+++ eauctions_scraper.py
@@ -51,6 +51,8 @@ class GrAuctionsScraper:
         options = webdriver.ChromeOptions()
         options.add_argument("--headless")
         options.add_argument("--no-sandbox")
+        options.add_argument("--disable-dev-shm-usage")
+        options.add_argument("--remote-debugging-port=9222")
 
         ua = UserAgent()
         userAgent = ua.random
